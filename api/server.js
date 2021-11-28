cc2530e0933d5ad05321ec009cbe459af069f9db
Mimmey <motyolya@gmail.com>
2021-11-28 14:17:56 +0300

    Updated server.js

--- api/server.js
+++ api/server.js
@@ -1,13 +1,29 @@
-import express from "express";
-import { json, urlencoded } from "body-parser";
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('quizzes', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+/*
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+*/
 
 const app = express();
 
 //делаем наш парсинг в формате json
-app.use(json());
+app.use(bodyParser.json());
 
 // парсит запросы по типу: application/x-www-form-urlencoded
-app.use(urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //  простой response - request
 app.get("/", (req, res) => {
