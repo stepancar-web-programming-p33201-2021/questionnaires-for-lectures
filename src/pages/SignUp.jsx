36e76c3cb2c5f8584d442a2691011d0f693040a6
Karo <snow.leopard.111@yandex.ru>
2021-12-14 14:55:48 +0300

    форма добавления вопросов и другое

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,8 @@
+import Auth from '../components/Auth';
+import { SIGN_UP_TITLE } from '../utils/constants';
+
+function SignUp() {
+  return <Auth title={SIGN_UP_TITLE} />;
+}
+
+export default SignUp;
